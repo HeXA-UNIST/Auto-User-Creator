e1b84f4f40dfa8ab21d7746b80122b9c89dc3817
Junwha Hong <junwha0511@naver.com>
2023-09-23 15:15:30 +0000

    Avoid halt on error

--- main.py
+++ main.py
@@ -46,75 +46,75 @@ def send_email(email_addr, uid, password):
     
 @bot.command("계정주세요")
 async def hi(ctx):
-    def check_instant_return(message):
-        return message.author == ctx.author and not message.author.bot 
+    try:
+        def check_instant_return(message):
+            return message.author == ctx.author and not message.author.bot 
 
-    # 1. Request and receive user email and person_name
-    await ctx.send("Please provide your person name:")
-    person_name = await bot.wait_for('message', timeout=60.0, check=check_instant_return)
-    person_name = person_name.content
+        # 1. Request and receive user email and person_name
+        await ctx.send("Please provide your person name:")
+        try: 
+            person_name = await bot.wait_for('message', timeout=60.0, check=check_instant_return)
+            person_name = person_name.content
 
-    await ctx.send("Please provide your email:")
-    try: 
-        email_msg = await bot.wait_for('message', timeout=60.0, check=check_instant_return)
-    except asyncio.TimeoutError:
-        await ctx.send("Timeout")
-        return
-    
-    email = email_msg.content
-
-    # 2.1 Validate email
-    retry = 0
-    while not re.match(r'^([\w.-]+)@unist\.ac\.kr$', email) and retry < MAXIMUM_RETRY:
-        await ctx.send("email must end with 'unist.ac.kr': {}".format(email))
-        await ctx.send("Please provide your email:")
-        try:
+            await ctx.send("Please provide your email:")
             email_msg = await bot.wait_for('message', timeout=60.0, check=check_instant_return)
+            email = email_msg.content
+
+            # 2.1 Validate email
+            retry = 0
+            while not re.match(r'^([\w.-]+)@unist\.ac\.kr$', email) and retry < MAXIMUM_RETRY:
+                await ctx.send("email must end with 'unist.ac.kr': {}".format(email))
+                await ctx.send("Please provide your email:")
+
+                email_msg = await bot.wait_for('message', timeout=60.0, check=check_instant_return)
+
+                email = email_msg.content
+                retry += 1
+            if retry == MAXIMUM_RETRY:
+                await ctx.send(f"Invalid format of email: {email}")
+                return
+            
         except asyncio.TimeoutError:
             await ctx.send("Timeout")
             return
-        email = email_msg.content
-        retry += 1
-    if retry == MAXIMUM_RETRY:
-        await ctx.send(f"Invalid format of email: {email}")
-        return
-    
-    # 2.2 Check if email already exists in DB
-    # c.execute("SELECT * FROM users WHERE email=?", (email,))
-    # existing_user = c.fetchone()
-    # if existing_user:
-    #     await ctx.send(f"The user {email}({existing_user[0]}) is already enrolled.")
-    #     return
-    
-    # 2.3 Extract the part before "@"
-    splitter_i = email.find("@") # assert != -1
-    username = email[:splitter_i]
+        
+        # 2.2 Check if email already exists in DB
+        # c.execute("SELECT * FROM users WHERE email=?", (email,))
+        # existing_user = c.fetchone()
+        # if existing_user:
+        #     await ctx.send(f"The user {email}({existing_user[0]}) is already enrolled.")
+        #     return
+        
+        # 2.3 Extract the part before "@"
+        splitter_i = email.find("@") # assert != -1
+        username = email[:splitter_i]
 
-    # 3.1 Create user on the server
-    password = random_password()
-    result = subprocess.run(['sudo', 'adduser', '--gecos', '', '--disabled-password', username], capture_output=True, text=True)
-    if result.returncode != 0:
-        await ctx.send(f"Error creating user: {result.stderr}")
-        return
-    
-    # 3.2 Set the password for the user
-    proc = subprocess.Popen(['passwd', username], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    stdout, stderr = proc.communicate(input=f"{password}\n{password}\n")
-    if proc.returncode != 0:
-        await ctx.send(f"Error setting password: {stderr}")
-        return
-    
-    # 4. Insert user info into the database
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    with conn:
-        c.execute("INSERT INTO users VALUES (?, ?, ?)", (person_name, email, current_time))
-		
-    # 5. Print user info
-    send_email(email, username, password)
-    await ctx.send(f"User {username}(owned by {person_name}) has been created on {current_time}!\nthe password was sent to {email}")
-    
-    # After successfully creating the user and inserting into the DB
-    target_channel = bot.get_channel(TARGET_CHANNEL_ID)
-    await target_channel.send(f"User {username}(owned by {person_name} / {email}) has been created on {current_time}.")
-
+        # 3.1 Create user on the server
+        password = random_password()
+        result = subprocess.run(['sudo', 'adduser', '--gecos', '', '--disabled-password', username], capture_output=True, text=True)
+        if result.returncode != 0:
+            await ctx.send(f"Error creating user: {result.stderr}")
+            return
+        
+        # 3.2 Set the password for the user
+        proc = subprocess.Popen(['passwd', username], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        stdout, stderr = proc.communicate(input=f"{password}\n{password}\n")
+        if proc.returncode != 0:
+            await ctx.send(f"Error setting password: {stderr}")
+            return
+        
+        # 4. Insert user info into the database
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        with conn:
+            c.execute("INSERT INTO users VALUES (?, ?, ?)", (person_name, email, current_time))
+            
+        # 5. Print user info
+        send_email(email, username, password)
+        await ctx.send(f"User {username}(owned by {person_name}) has been created on {current_time}!\nthe password was sent to {email}")
+        
+        # After successfully creating the user and inserting into the DB
+        target_channel = bot.get_channel(TARGET_CHANNEL_ID)
+        await target_channel.send(f"User {username}(owned by {person_name} / {email}) has been created on {current_time}.")
+    except Exception as e:
+        print(e)
 bot.run(TOKEN)
